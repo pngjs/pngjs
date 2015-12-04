1ba93c2af83422c399c335e3056775bd2de6282a
Luke Page <luke.a.page@gmail.com>
2015-12-04 21:21:29 +0000

    Fix newfile example. Fixes #35

--- examples/newfile.js
+++ examples/newfile.js
@@ -1,23 +1,23 @@
 var PNG = require("../lib/png").PNG;
+var fs = require("fs");
 
 var newfile = new PNG({width:10,height:10});
 
-for (var y = 0; y < png.height; y++) {
-  for (var x = 0; x < png.width; x++) {
-    var idx = (png.width * y + x) << 2;
+for (var y = 0; y < newfile.height; y++) {
+  for (var x = 0; x < newfile.width; x++) {
+    var idx = (newfile.width * y + x) << 2;
 
-    var col = x < (png.width >> 1) ^ y < (png.height >> 1) ? 0xe5 : 0xff;
+    var col = x < (newfile.width >> 1) ^ y < (newfile.height >> 1) ? 0xe5 : 0xff;
 
-    png.data[idx] = col;
-    png.data[idx + 1] = col;
-    png.data[idx + 2] = col;
-    png.data[idx + 3] = 0xff;
+    newfile.data[idx] = col;
+    newfile.data[idx + 1] = col;
+    newfile.data[idx + 2] = col;
+    newfile.data[idx + 3] = 0xff;
   }
 }
 
 newfile.pack()
   .pipe(fs.createWriteStream(__dirname + '/newfile.png'))
   .on('finish', function() {
-    console.log('bitmap:', this.data);
-    console.log('end !');
+    console.log('Written!');
   });
