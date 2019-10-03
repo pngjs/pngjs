89b7faab5a9ee1b07b502f0eea264ba924fa798c
ossdev <ossdev@puresoftware.com>
2019-10-03 17:38:41 +0530

    pngjs: Replace phantomjs dependency with chromium puppeteer
    Signed-off-by: ossdev <ossdev@puresoftware.com>

--- test/run-compare.js
+++ test/run-compare.js
@@ -1,24 +1,42 @@
-require("./http-server");
+require('./http-server')
+const puppeteer = require('puppeteer');
+const URL = 'http://localhost:8000';
 
-var path = require('path');
-var childProcess = require('child_process');
-var phantomjs = require('phantomjs-prebuilt');
-var binPath = phantomjs.path;
-
-var childArgs = [
-  path.join(__dirname, 'phantom-compare.js')
-];
-
-try {
-  console.log("Comparing in PhantomJS");
-
-  childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
-
-    // handle results
+puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }).then(async browser => {
+    const page = await browser.newPage();
+    await page.goto(URL, {waitUntil: 'networkidle0'});
+    const results = await page.evaluate(() => {
+    try {
+	if (window.isFinished && window.isFinished()) {
+	return window.results;
+    }
+    } catch(err) {
+       reject(err.toString());
+       console.log("Failed", err);
+    }
+    });
+    console.log("Comparing in Chrome");
     console.log("Comparison Test Results:");
-    console.log(stdout);
-    process.exit(err ? 1 : 0);
-  });
-} catch (e) {
-  console.log("Error starting phantomjs");
-}
+    await browser.close();
+    if (results) {
+	      var success = true;
+	      var successes = [],failures = [];
+	      for (var i = 0; i < results.length; i++) {
+	            var result = results[i];
+	            if (result.success) {
+		            successes.push(result.name);
+	            } else {
+		          failures.push(result.name);
+	            }
+	            success = success && result.success;
+	       }
+	      console.log("Success:", successes.join(", "));
+	      if (failures.length) {
+	            console.log("Failure:", failures.join(", "));
+	      }
+    }
+    process.exit();
+}).catch(function(error) {
+    console.error(error);
+    process.exit();
+});
