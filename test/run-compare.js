b9d37c13624832a22b5a9bdd453d4b02a1bb5d52
ossdev <ossdev@puresoftware.com>
2019-10-03 18:03:30 +0530

    pngjs: Replace phantomjs dependency with chromium puppeteer
    Signed-off-by: ossdev <ossdev@puresoftware.com>

--- test/run-compare.js
+++ test/run-compare.js
@@ -6,34 +6,34 @@ puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sand
     const page = await browser.newPage();
     await page.goto(URL, {waitUntil: 'networkidle0'});
     const results = await page.evaluate(() => {
-    try {
+      try {
 	if (window.isFinished && window.isFinished()) {
-	return window.results;
-    }
-    } catch(err) {
-       reject(err.toString());
-       console.log("Failed", err);
-    }
+	  return window.results;
+	}
+      } catch(err) {
+        reject(err.toString());
+        console.log("Failed", err);
+      }
     });
     console.log("Comparing in Chrome");
     console.log("Comparison Test Results:");
     await browser.close();
     if (results) {
-	      var success = true;
-	      var successes = [],failures = [];
-	      for (var i = 0; i < results.length; i++) {
-	            var result = results[i];
-	            if (result.success) {
-		            successes.push(result.name);
-	            } else {
-		          failures.push(result.name);
-	            }
-	            success = success && result.success;
-	       }
-	      console.log("Success:", successes.join(", "));
-	      if (failures.length) {
-	            console.log("Failure:", failures.join(", "));
-	      }
+      var success = true;
+      var successes = [],failures = [];
+      for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+	if (result.success) {
+	  successes.push(result.name);
+	} else {
+	  failures.push(result.name);
+	}
+	success = success && result.success;
+      }
+      console.log("Success:", successes.join(", "));
+      if (failures.length) {
+        console.log("Failure:", failures.join(", "));
+      }
     }
     process.exit();
 }).catch(function(error) {
